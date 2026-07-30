9f7abfcfdcb66a5415ac06a4f9468cb6db1ad4f3
Ker ZJZ <111036322+kerzjz@users.noreply.github.com>
2026-07-30 17:54:19 +0800

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -54,6 +54,10 @@ export const siteConfig = {
   ],
 };
 
+// 兼容旧项目 info.ts 所需导出
+export const bannerConfig = {};
+export const subtitleConfig = {};
+
 // ===================== 预处理Waline 避免对象内部直接运算引发Astro构建异常 =====================
 let walineDnsPrefetch: string[] = [];
 let walinePreconnect: Array<{ url: string; crossOrigin: string }> = [];
